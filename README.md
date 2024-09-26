a99edb677728de2df90d5e9daa1a6f162f7e0af6
DJReflexive <89596189+DJReflexive@users.noreply.github.com>
2024-09-26 11:43:50 -0600

    Submission 1, Changes to code.js & README

--- README.md
+++ README.md
@@ -16,6 +16,31 @@ its worst-case runtime. Add your answer, including your reasoning, to this
 markdown file.
 
 
+## My Runtime Analysis
+
+First, addressing the base case where the length is equal to 0 or 1, we actually
+get a constant run time since an array that is empty or only has 1 element is
+already sorted. However, we are going to look at the more general case where
+the length is greater than 1.
+
+Going into the first loop, the program will run a time complexity of $\Theta(n)$ 
+to process every element in the array (looping through groupings, which the 
+groupings are then also looped through with nested loop). 
+
+Then pairs of groupings are compared in the merge() method. The two groupings are 
+then iterated through (at the same time, simultaneously), and it checked if elements 
+are out of order by cross comparing between the two groups. Since we are interested 
+in the worst case scenerio, we are going to assume that the data is ordered in such 
+a way that there is a swap occuring every iteration. This process executes with a
+time complexity of $\Theta(n)$ since every element has a swap call. If were not 
+assuming that the swap call is executed every iteration, it would actually resemble,
+$\Theta(logn)$, however, we are not interested in this case for this analysis.
+
+Combining the two time complexities, we see that the worst case complexity is 
+$\Theta(n^2)$. In the average case, the final complexity would resemble $\Theta(nlogn)$, however, since we are assuming that the merge function swap on every 
+iteration, this increases the complexity. But since we are only interested in the
+worst case scenerio, $\Theta(n^2)$ is the final complexity.
+
 # Sources
 
 - geeksforgeeks.org - for help with the merge function. 
--- code.js
+++ code.js
@@ -1,117 +1,42 @@
-// Swaps the two element at the given indexes of the array
-function swap(aIndex, bIndex, array) {
-    let temp;
 
-    temp = array[aIndex];
-    array[aIndex] = array[bIndex];
-    array[bIndex] = temp;
+function merge(left, mid, end, arr) {
+    let i = left; // Left Grouping Index
+    let j = mid+1;  // Right Grouping Index
 
-    return array;
+    // looping through the two groupings simultaneously
+    while (i <= mid && j <= end) {
+        if (arr[i] <= arr[j]) { i++; } // do nothing, iterate to next element
+        else { // swap elements to correct positions
+            let tmp = arr[j];
+
+            // Move elements to the right
+            for (let k = j; k > i; k--) { arr[k] = arr[k-1]; }
+
+            arr[i] = tmp;
+
+            i++;
+            mid++;
+            j++;
+        }
+    }
+
+    return arr;
 }
 
-
-// // Merges the two groupings together
-// function merge(leftIndex, mid, rightIndex, array) {
-//     let i, j, k
-//     let leftLen  = mid - leftIndex + 1;
-//     let rightLen = rightIndex - mid;
-
-    // let leftArr  = Array(leftLen);
-    // let rightArr = Array(rightLen);
-
-//     leftArr  = array.slice(leftIndex, mid+1);
-//     rightArr = array.slice(mid, rightIndex+1);
-
-
-//     // Merge temp arrays into array
-//     i = 0;
-//     j = 0;
-//     k = 0;
-//     while (i < leftLen && j < rightLen) {
-//         if (leftArr[i] <= rightArr[j]) {
-//             arr[k] = leftArr[i];
-//             i++
-//         } else {
-//             arr[k] = rightArr[j];
-//             j++;
-//         }
-//         k++
-//     }
-
-//     // Copy any extra elements
-//     while (i < leftLen) {
-//         arr[k] = leftArr[i];
-//         i++;
-//         k++;
-//     }
-//     while (j < rightLen) {
-//         arr[k] = rightArr[j];
-//         j++;
-//         k++;
-//     }
-// }
-
-
-// An Iterative Merge Sort Algorithm
 function mergesort(array) {
     let len = array.length;
+    if (len <= 1){ return array; } // Return array if len is 0 or 1
 
-    // If array is empty or 1 element
-    if (len == 0 || len == 1){ return array; } // might not be needed
-
-
-    // If array is larger than 1
-    for (let groupings = 1; groupings*2 <= len; groupings *= 2) {
-        // The two indexes that mark the start of each grouping
-        let i = 0;
-        let j = groupings;
-
-        // checks if the group sizes are running off the array
-        while (i+groupings <= len && j+groupings <= len) {
-            // extra iterators to help i & j iterate through the array
-            let k = 0;
-            let l = 0;
-
-            // Iterates through the two groupings and see if need to be swapped
-            while (k <= groupings && l < groupings) {
-                // Checks which element is larger
-                if (array[i+k] <= array[j+l]) { 
-                    // do nothing, elements already sorted
-                    k++;
-                }
-                else { 
-                    swap(i+k, j+l, array);
-
-                    /* 
-                        After initial swap, need to check if the elements swapped
-                        are no longer sorted. If no longer sorted, swap the first 
-                        two elements in the second grouping. This does NOT happen
-                        if the indexes are outside of the scope of the groupings.
-                    */
-                    // // Left Grouping
-                    // if (array[i+k] > array[i+k+1] && 0 == (i+k) % groupings && groupings > 1) {
-                    //     swap(i+k, i+k+1, array);
-                    // }
-
-                    // // Right Grouping
-                    // if (array[j+l] > array[j+l+1] && 0 == (j+l) % groupings && groupings > 1) {
-                    //     swap(j+l, j+l+1, array);
-                    // }
-
-                    l++;
-                }              
-
-            }
-
-            i += groupings*2;
-            j += groupings*2;
+    // cycles through all groupings
+    for (let grouping = 1; grouping < len; grouping *= 2) {
+        // loops through individual groupings
+        for (let i = 0; i < len; i += 2*grouping) {
+            let mid = (i+grouping-1 < len-1) ? i+grouping-1 : len-1; // left grouping
+            let end = (i+2*grouping-1 < len-1) ? i+2*grouping-1 : len-1; // right grouping
+            // merge the left and right groupings
+            merge(i, mid, end, array);
         }
     }
 
     return array;
 }
-
-
-
-test0 = [8,6,2,3,4,9,7,1,0,5];
-console.log("test0: [" + mergesort(test0) + "]");
